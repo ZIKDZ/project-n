7b886b7320c622df01abd5ab6921e1013bc9f60e
ILIAS <iliase.zaoui@gmail.com>
2025-02-05 06:33:48 +0100

    Added some ui

--- pages/forms.py
+++ pages/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Player, Rank, Game  # Assuming you have a Game model as well
+from .models import Player, Rank, Game
+from django.core.exceptions import ValidationError 
 
 class PlayerForm(forms.ModelForm):
     class Meta:
@@ -21,3 +22,26 @@ class PlayerForm(forms.ModelForm):
 
         # Ensure that the game field has the correct ID for JavaScript
         self.fields['game'].widget.attrs['id'] = 'game-dropdown'
+    
+    # Errors Logging
+
+    def clean_username(self):
+            username = self.cleaned_data.get('username')
+            if Player.objects.filter(username=username).exists():
+                raise ValidationError("This username is already taken.")
+            return username 
+    def clean_in_game_username(self):
+        in_game_username = self.cleaned_data.get('in_game_username')
+        if Player.objects.filter(in_game_username=in_game_username).exists():
+            raise ValidationError("This in-game username is already taken.")
+        return in_game_username 
+    def clean_discord_username(self):
+        discord_username = self.cleaned_data.get('discord_username')
+        if Player.objects.filter(discord_username=discord_username).exists():
+            raise ValidationError("This Discord username is already in use.")
+        return discord_username 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if Player.objects.filter(email=email).exists():
+            raise ValidationError("This email is already registered.")
+        return email
